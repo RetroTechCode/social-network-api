54a1954179e776cd728bdad2a9f1fcdf58b5097e
Dan <danaaboston@gmail.com>
2023-03-09 15:20:47 -0500

    Set up schema for Thoughts and added dayjs package

--- models/Thought.js
+++ models/Thought.js
@@ -1,2 +1,38 @@
 const { Schema, model } = require('mongoose');
+const dayjs = require('dayjs');
 
+const thoughtSchema = new schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtTime => dayjs(createdAtTime).format('MMM DD, YYYY [at] hh:mm a')
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [reactionSchema]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+thoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
+
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
