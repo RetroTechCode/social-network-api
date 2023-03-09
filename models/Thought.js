41f96297e2c5f052c80f10d723be28a0fb440d6c
Dan <danaaboston@gmail.com>
2023-03-09 15:32:27 -0500

    Deleted /models/Reaction.js and built schema for reactions within /models/Thought.js. Corrected 'schema' to 'Schema' for creating new schema for thoughts

--- models/Thought.js
+++ models/Thought.js
@@ -1,7 +1,7 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model, Types } = require('mongoose');
 const dayjs = require('dayjs');
 
-const thoughtSchema = new schema(
+const thoughtSchema = new Schema(
     {
         thoughtText: {
             type: String,
@@ -28,6 +28,36 @@ const thoughtSchema = new schema(
         id: false
     }
 );
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxLength: 280
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtTime => dayjs(createdAtTime).format('MMM DD, YYYY [at] hh:mm a')
+        }
+    },
+    {
+        toJSON: {
+            getters: true
+        },
+        id: false
+    }
+);
+
 
 thoughtSchema.virtual('reactionCount').get(function() {
     return this.reactions.length;
