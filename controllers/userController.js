9ea9567e5eb57c47a7edb97189d2945e23bcaf47
Dan <danaaboston@gmail.com>
2023-03-10 11:53:39 -0500

    Began setting up userController

---
+++ controllers/userController.js
@@ -0,0 +1,25 @@
+const { User, Thought } = require('../models');
+
+const userController = {
+    getUsers(req, res) {
+
+    },
+    getSingleUser(req, res) {
+
+    },
+    createUser(req, res) {
+
+    },
+    updateUser(req, res) {
+
+    },
+    deleteUser(req, res) {
+
+    },
+    addFriend(req, res) {
+
+    },
+    removeFriend(req, res) {
+        
+    }
+}
