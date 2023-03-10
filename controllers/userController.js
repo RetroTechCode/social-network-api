7ece6f7a4ebebe8b5b9616fd2e9c282a0073daea
Dan <danaaboston@gmail.com>
2023-03-10 12:53:16 -0500

    Added functionality for getUsers, getSingleUser, and createUser within userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,12 +2,24 @@ const { User, Thought } = require('../models');
 
 const userController = {
     getUsers(req, res) {
+        User.find()
+            .then((data) => res.JSON(data))
+            .catch((err) => res.status(500).JSON(err));
     },
     getSingleUser(req, res) {
-
+        User.findOne({ _id: req.params.userId })
+        .then((data) => {
+            if(!data) {
+                res.status(404).JSON({ message: 'Error - User not found' });
+            }
+            res.JSON(data);
+        })
+        .catch((err) => res.status(500).JSON(err));
     },
     createUser(req, res) {
-
+        User.create(req.body)
+        .then((data) => res.JSON(data))
+        .catch((err) => res.status(500).JSON(err));
     },
     updateUser(req, res) {
 
