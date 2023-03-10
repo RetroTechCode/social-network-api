07b8d6ff37a75f445baa04fd9b44e1e12f0e0575
Dan <danaaboston@gmail.com>
2023-03-10 15:39:07 -0500

    Updated thoughtController to allow for getting all and individual thoughts, creating thoughts, updating thoughts, and deleting thoughts

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -2,19 +2,46 @@ const { User, Thought } = require('../models');
 
 const thoughtController = {
     getThoughts(req, res) {
-
+        Thought.find()
+            .select('-__v')
+            .then((data) => res.json(data))
+            .catch((err) => res.status(500).json(err));
     },
     getSingleThought(req, res) {
-
+        Thought.findOne({ _id: req.params.thoughtId })
+            .select('-__v')
+            .then((data) => {
+                if (!data) {
+                    res.status(404).json({ message: 'Error - Thought not found' });
+                }
+                res.json(data);
+            })
+            .catch((err) => res.status(500).json(err));
     },
     createThought(req, res) {
-
+        Thought.create(req.body)
+            .then((data) => res.json(data))
+            .catch((err) => res.status(500).json(err));
     },
     updateThought(req, res) {
-
+        Thought.findOneAndUpdate({ _id: req.params.thoughtId }, { $set: req.body }, { runValidators: true, new: true })
+            .then((data) => {
+                if (!data) {
+                    res.status(404).json({ message: 'Error - Thought not found' })
+                }
+                res.json(data)
+            })
+            .catch((err) => res.status(500).json(err));
     },
     deleteThought(req, res) {
-
+        Thought.findOneAndDelete({ _id: req.params.thoughtId })
+            .then((data) => {
+                if (!data) {
+                    res.status(404).json({ message: 'Error - Thought not found' })
+                }
+                res.json(data)
+            })
+            .catch((err) => res.status(500).json(err));
     },
     createReaction(req, res) {
 
