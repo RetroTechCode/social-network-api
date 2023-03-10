c72455f3a245b3279409d39830fb80110b3c5b38
Dan <danaaboston@gmail.com>
2023-03-10 13:57:04 -0500

    Fixed missing () on line 9 of server.js, fixed reactionSchema to run before thoughtSchema within /models/Thought.js, and further updated functionality within /controllers/userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,35 +3,59 @@ const { User, Thought } = require('../models');
 const userController = {
     getUsers(req, res) {
         User.find()
-            .then((data) => res.JSON(data))
-            .catch((err) => res.status(500).JSON(err));
+            .then((data) => res.json(data))
+            .catch((err) => res.status(500).json(err));
     },
     getSingleUser(req, res) {
         User.findOne({ _id: req.params.userId })
-        .then((data) => {
-            if(!data) {
-                res.status(404).JSON({ message: 'Error - User not found' });
-            }
-            res.JSON(data);
-        })
-        .catch((err) => res.status(500).JSON(err));
+            .then((data) => {
+                if (!data) {
+                    res.status(404).json({ message: 'Error - User not found' });
+                }
+                res.json(data);
+            })
+            .catch((err) => res.status(500).json(err));
     },
     createUser(req, res) {
         User.create(req.body)
-        .then((data) => res.JSON(data))
-        .catch((err) => res.status(500).JSON(err));
+            .then((data) => res.json(data))
+            .catch((err) => res.status(500).json(err));
     },
     updateUser(req, res) {
-
+        User.findOneAndUpdate({ id_: req.params.userId }, { $set: req.body }, { runValidators: true, new: true })
+            .then((data) => {
+                if (!data) {
+                    res.status(404).json({ message: 'Error - User not found' })
+                }
+            })
+            .catch((err) => res.status(500).json(err));
     },
     deleteUser(req, res) {
-
+        User.findOneAndDelete({ _id: req.params.userId })
+            .then((data) => {
+                if (!data) {
+                    res.status(404).json({ message: 'Error - User not found' })
+                }
+            })
+            .catch((err) => res.status(500).json(err));
     },
     addFriend(req, res) {
-
+        User.findOneAndUpdate({ _id: req.params.userId }, { $addToSet: { friends: req.params.friendId } }, { new: true })
+            .then((data) => {
+                if (!data) {
+                    res.status(404).json({ message: 'Error - User not found' })
+                }
+            })
+            .catch((err) => res.status(500).json(err));
     },
     removeFriend(req, res) {
-
+        User.findOneAndUpdate({ _id: req.params.userId }, { $pull: { friends: req.params.friendId } }, { new: true })
+            .then((data) => {
+                if (!data) {
+                    res.status(404).json({ message: 'Error - User not found' })
+                }
+            })
+            .catch((err) => res.status(500).json(err));
     }
 };
 
--- models/Thought.js
+++ models/Thought.js
@@ -1,34 +1,6 @@
 const { Schema, model, Types } = require('mongoose');
 const dayjs = require('dayjs');
 
-const thoughtSchema = new Schema(
-    {
-        thoughtText: {
-            type: String,
-            required: true,
-            minlength: 1,
-            maxlength: 280
-        },
-        createdAt: {
-            type: Date,
-            default: Date.now,
-            get: createdAtTime => dayjs(createdAtTime).format('MMM DD, YYYY [at] hh:mm a')
-        },
-        username: {
-            type: String,
-            required: true
-        },
-        reactions: [reactionSchema]
-    },
-    {
-        toJSON: {
-            virtuals: true,
-            getters: true
-        },
-        id: false
-    }
-);
-
 const reactionSchema = new Schema(
     {
         reactionId: {
@@ -58,6 +30,33 @@ const reactionSchema = new Schema(
     }
 );
 
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtTime => dayjs(createdAtTime).format('MMM DD, YYYY [at] hh:mm a')
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [reactionSchema]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
 
 thoughtSchema.virtual('reactionCount').get(function() {
     return this.reactions.length;
