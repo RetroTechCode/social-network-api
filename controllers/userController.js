29055c492239402adfaa0e0b001e5efbce424364
Dan <danaaboston@gmail.com>
2023-03-10 15:18:22 -0500

    Updated addFriend and removeFriend functions in userController.js to respond with the user being modified - All user routes appear to be functioning at this point

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,11 +3,13 @@ const { User, Thought } = require('../models');
 const userController = {
     getUsers(req, res) {
         User.find()
+            .select('-__v')
             .then((data) => res.json(data))
             .catch((err) => res.status(500).json(err));
     },
     getSingleUser(req, res) {
         User.findOne({ _id: req.params.userId })
+            .select('-__v')
             .then((data) => {
                 if (!data) {
                     res.status(404).json({ message: 'Error - User not found' });
@@ -47,6 +49,7 @@ const userController = {
                 if (!data) {
                     res.status(404).json({ message: 'Error - User not found' })
                 }
+                res.json(data)
             })
             .catch((err) => res.status(500).json(err));
     },
@@ -56,6 +59,7 @@ const userController = {
                 if (!data) {
                     res.status(404).json({ message: 'Error - User not found' })
                 }
+                res.json(data)
             })
             .catch((err) => res.status(500).json(err));
     }
